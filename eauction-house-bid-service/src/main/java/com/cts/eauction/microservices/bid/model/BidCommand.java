7cb39d1100733e0e5d31c5e050d4a70a4138fbcd
supratim78 <supratim78@gmail.com>
2022-05-14 07:58:37 +0530

    initial changes

---
+++ eauction-house-bid-service/src/main/java/com/cts/eauction/microservices/bid/model/BidCommand.java
@@ -0,0 +1,115 @@
+package com.cts.eauction.microservices.bid.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="Bid")
+public class BidCommand {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+	@GenericGenerator(name = "native",strategy = "native")
+	private Integer id;
+	private Integer productId;
+	private Integer bidAmount;
+	private Integer buyerId;
+
+	public BidCommand() {
+
+	}
+
+	public BidCommand(Integer id, Integer productId, Integer bidAmount, Integer buyerId) {
+		super();
+		this.id = id;
+		this.productId = productId;
+		this.bidAmount = bidAmount;
+		this.buyerId = buyerId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Integer getBidAmount() {
+		return bidAmount;
+	}
+
+	public void setBidAmount(Integer bidAmount) {
+		this.bidAmount = bidAmount;
+	}
+
+	public Integer getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(Integer buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bidAmount == null) ? 0 : bidAmount.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result + ((buyerId == null) ? 0 : buyerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BidCommand other = (BidCommand) obj;
+		if (bidAmount == null) {
+			if (other.bidAmount != null)
+				return false;
+		} else if (!bidAmount.equals(other.bidAmount))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		if (buyerId == null) {
+			if (other.buyerId != null)
+				return false;
+		} else if (!buyerId.equals(other.buyerId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Bid [id=" + id + ", productid=" + productId + ", bidAmount=" + bidAmount + ", sellerId=" + buyerId
+				+ "]";
+	}
+}
