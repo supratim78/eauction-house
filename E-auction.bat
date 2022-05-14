9ebb3d29ef166f5c25b538ae608df69ea9967e72
supratim78 <supratim78@gmail.com>
2022-05-14 07:56:20 +0530

    Initial Changes

---
+++ E-auction.bat
@@ -0,0 +1,12 @@
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-configuration-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-discovery-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-gateway-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-auth-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-seller-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-product-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-buyer-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-bid-service && gradle bootRun"
+start cmd /k "cd D:\Work\HackFSE-2022-v1\project-repo\eauction-house\eauction-house-listing-service && gradle bootRun"
+
+
+
