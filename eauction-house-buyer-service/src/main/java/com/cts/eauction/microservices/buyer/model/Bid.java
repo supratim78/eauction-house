2042a86c9ee87a9734d83e5a5912629871a7db14
supratim78 <45755585+supratim78@users.noreply.github.com>
2022-06-25 17:01:35 +0530

    Update Bid.java

--- eauction-house-buyer-service/src/main/java/com/cts/eauction/microservices/buyer/model/Bid.java
+++ eauction-house-buyer-service/src/main/java/com/cts/eauction/microservices/buyer/model/Bid.java
@@ -6,17 +6,24 @@ public class Bid {
 	private Integer productId;
 	private Integer bidAmount;
 	private Integer buyerId;
+	private String buyerName;
+	private String buyerPhone;
+	private String buyerEmail;
 
 	public Bid() {
 
 	}
 
-	public Bid(Integer id, Integer productId, Integer bidAmount, Integer buyerId) {
+	public Bid(Integer id, Integer productId, Integer bidAmount, Integer buyerId, String buyerName, String buyerPhone,
+			String buyerEmail) {
 		super();
 		this.id = id;
 		this.productId = productId;
 		this.bidAmount = bidAmount;
 		this.buyerId = buyerId;
+		this.buyerName = buyerName;
+		this.buyerPhone = buyerPhone;
+		this.buyerEmail = buyerEmail;
 	}
 
 	public Integer getId() {
@@ -50,15 +57,42 @@ public class Bid {
 	public void setBuyerId(Integer buyerId) {
 		this.buyerId = buyerId;
 	}
+	
+	public String getBuyerName() {
+		return buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	public String getBuyerPhone() {
+		return buyerPhone;
+	}
+
+	public void setBuyerPhone(String buyerPhone) {
+		this.buyerPhone = buyerPhone;
+	}
+
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+
+	public void setBuyerEmail(String buyerEmail) {
+		this.buyerEmail = buyerEmail;
+	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((bidAmount == null) ? 0 : bidAmount.hashCode());
+		result = prime * result + ((buyerEmail == null) ? 0 : buyerEmail.hashCode());
+		result = prime * result + ((buyerId == null) ? 0 : buyerId.hashCode());
+		result = prime * result + ((buyerName == null) ? 0 : buyerName.hashCode());
+		result = prime * result + ((buyerPhone == null) ? 0 : buyerPhone.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
-		result = prime * result + ((buyerId == null) ? 0 : buyerId.hashCode());
 		return result;
 	}
 
@@ -76,6 +110,26 @@ public class Bid {
 				return false;
 		} else if (!bidAmount.equals(other.bidAmount))
 			return false;
+		if (buyerEmail == null) {
+			if (other.buyerEmail != null)
+				return false;
+		} else if (!buyerEmail.equals(other.buyerEmail))
+			return false;
+		if (buyerId == null) {
+			if (other.buyerId != null)
+				return false;
+		} else if (!buyerId.equals(other.buyerId))
+			return false;
+		if (buyerName == null) {
+			if (other.buyerName != null)
+				return false;
+		} else if (!buyerName.equals(other.buyerName))
+			return false;
+		if (buyerPhone == null) {
+			if (other.buyerPhone != null)
+				return false;
+		} else if (!buyerPhone.equals(other.buyerPhone))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -86,17 +140,12 @@ public class Bid {
 				return false;
 		} else if (!productId.equals(other.productId))
 			return false;
-		if (buyerId == null) {
-			if (other.buyerId != null)
-				return false;
-		} else if (!buyerId.equals(other.buyerId))
-			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Bid [id=" + id + ", productid=" + productId + ", bidAmount=" + bidAmount + ", sellerId=" + buyerId
-				+ "]";
+		return "Bid [id=" + id + ", productId=" + productId + ", bidAmount=" + bidAmount + ", buyerId=" + buyerId
+				+ ", buyerName=" + buyerName + ", buyerPhone=" + buyerPhone + ", buyerEmail=" + buyerEmail + "]";
 	}
 }
